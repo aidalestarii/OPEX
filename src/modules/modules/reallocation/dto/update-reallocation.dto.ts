863552b3a33024bf527783b5607ca7f57e1455ea
xyz-loops <marcelinus.fajar@student.com>
2023-11-01 09:26:44 +0700

    [UPDATE] PrismaSchema1

--- src/modules/modules/reallocation/dto/update-reallocation.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateReallocationDto } from './create-reallocation.dto';
-
-export class UpdateReallocationDto extends PartialType(CreateReallocationDto) {
-          personalNumber: string;
-}
--- src/modules/modules/reallocation/reallocation.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ReallocationService } from './reallocation.service';
-import { ReallocationController } from './reallocation.controller';
-
-@Module({
-  controllers: [ReallocationController],
-  providers: [ReallocationService],
-})
-export class ReallocationModule {}
